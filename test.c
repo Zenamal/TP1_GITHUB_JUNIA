ad163512390744680acd2db548021bac7fdee2a1
Malo <malo.hamon@student.junia.com>
2025-10-13 13:41:08 +0200

    premier commit

---
+++ test.c
@@ -0,0 +1,10 @@
+int main(){
+    printf("No cost too great");
+    printf("No mind to think");
+    printf("No will to Break");
+    printf("No voice to cry suffering");
+    printf("Born of God and Void");
+    printf("You shall seal the blinding light that plagues their dreams");
+    printf("You are the Vessel");
+    printf("You are Hollow Knight");
+}
