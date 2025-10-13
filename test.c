4971e4e536e4b27f2e4b7e0f383a263c159f1c12
Malo <malo.hamon@student.junia.com>
2025-10-13 14:16:35 +0200

    2e commit

--- test.c
+++ test.c
@@ -1,10 +1,21 @@
+#include <stdio.h>
+//Exercice 1
 int main(){
-    printf("No cost too great");
-    printf("No mind to think");
-    printf("No will to Break");
-    printf("No voice to cry suffering");
-    printf("Born of God and Void");
-    printf("You shall seal the blinding light that plagues their dreams");
-    printf("You are the Vessel");
-    printf("You are Hollow Knight");
+int original = 0;
+scanf("%d",&original);
+int seconde = original;
+int heures = seconde/3600;
+seconde = seconde - (3600*heures);
+int minutes = seconde/60;
+seconde = seconde - (60*minutes);
+printf("%d secondes = %d heures, %d minutes, %d secondes",original,heures,minutes,seconde);
+return 0;
+}
+
+//Exercice 2
+int main(){
+int C = 0;
+int T = 0;
+int
+
 }
