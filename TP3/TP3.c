d90b4435ff96fcd937725e1b9b093e4d6b247420
Malo <malo.hamon@student.junia.com>
2025-11-17 14:29:10 +0100

    commit etape 4

--- TP3/TP3.c
+++ TP3/TP3.c
@@ -7,17 +7,42 @@ int main(){
     scanf("%d",&n);
     int width = n*n;
     int max_height = 3*n -1;
-    char img[20][20];
-    for(int i = 0;i < 20;i++)
+    char img[100][100];
+    for(int i = 0;i < 100;i++)
     {
-        for(int j = 0; j < 20;j++)
+        for(int j = 0; j < 100;j++)
         {
-            img[i][j]='a';
+            img[i][j]=' ';
         }
     }
-    for(int i=0;i<20;i++)
+    int mid=width/2;
+    int j = 50;
+    for(int i = 0; i< n; i++)
     {
-        for(int j = 0; j < 20;j++)
+        img[i][j]='A';
+        img[i][100-j]='A';
+        int k=j+1;
+        for(int k = j;k<100-j;k++)
+        {
+            img[i][k]='S';
+        }
+        j = j-1;
+        if(i==n-1)
+        {
+            int x=0;
+            int y=100;
+            while(x!=j)
+            {
+                img[i][x]='_';
+                img[i][y]='_';
+                x=x+1;
+                y=y-1;
+            }
+        }
+    }
+    for(int i=0;i<100;i++)
+    {
+        for(int j = 0; j < 100;j++)
         {
             printf("%c",img[i][j]);
         }
