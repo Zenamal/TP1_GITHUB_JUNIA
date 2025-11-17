84ed9cff8d95be016c25af4eb7fd677858f17e6f
Malo <malo.hamon@student.junia.com>
2025-11-17 13:43:23 +0100

    commit etape 2

---
+++ TP3/TP3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+int main(){
+    int n = 0;
+    printf("Choisissez la taille de l'etoile (Recommande de 5 a 10)");
+    scanf("%d",&n);
+    int width = n*n;
+    int max_height = 3*n -1;
+    char img[20][20];
+    for(int i = 0;i < 20;i++)
+    {
+        for(int j = 0; j < 20;j++)
+        {
+            img[i][j]='\0';
+        }
+    }
+}
