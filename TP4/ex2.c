379634c82803f54e21972a6dd4fe2575a03b01ae
Malo <malo.hamon@student.junia.com>
2025-11-26 16:32:20 +0100

    tp4 ex2 etape 5 et fin

--- TP4/ex2.c
+++ TP4/ex2.c
@@ -17,39 +17,58 @@ int saisir_montant(){
 }
 
 int montant_valide(int montant){
-    if (montant%5 == 0 && 5<montant<1000){
+    if (montant%5 == 0 && 5<montant && montant<1000){
         return 0;
     }
     else{
-        printf("Erreur : montant invalide\n Le montant doit etre entre 5 et 1000, divisible par 5.\n");
+        printf("Erreur : montant invalide\nLe montant doit etre entre 5 et 1000, divisible par 5.\n");
         return 1;
     }
     
 }
 
 void calcul_distribution(int montant){
-    printf("%d\n",montant);
     int gros_b = 0;
     int moyen_b = 0;
     int petit_b = 0;
-    while((montant - 50)>0){
+    while((montant - 50)>=0){
         gros_b++;
         montant = montant - 50;
-        printf("%d\n",gros_b);
     }
-    while((montant - 20)>0){
+    while((montant - 20)>=0){
         moyen_b++;
         montant = montant - 20;
     }
-    while((montant - 5)>0){
+    while((montant - 5)>=0){
         petit_b++;
         montant = montant - 5;
     }
-    printf("Billets distribues : \n%d Billets de 50 euros\n%d Billets de 20 euros\n%d Billets de 5 euros\n");
+    printf("Billets distribues : \n%d Billets de 50 euros\n%d Billets de 20 euros\n%d Billets de 5 euros\n",gros_b,moyen_b,petit_b);
 }
 
 int main(){
     int choix = 0;
+    while(choix!=2){
+    afficherMenu();
     printf("Votre choix : ");
     scanf("%d",&choix);
+    if (choix == 1){
+        int montant = 0;
+        int valide = -1;
+        while (valide != 0){
+            int montant = saisir_montant();
+            valide = montant_valide(montant);
+            if (valide == 0){
+            calcul_distribution(montant);
+            }
+        }
+    }
+    else if (choix == 2){
+        printf("Merci d'avoir utilise nos services. A bientot");
+    }
+
+    else{
+        printf("Aucune option ne correspond, veuillez reessayer\n");
+    }
+}
 }
