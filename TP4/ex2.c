93553a3c2629b502baf34f9355f25a45ca2d2b7e
Malo <malo.hamon@student.junia.com>
2025-11-26 16:05:45 +0100

    tp4 ex2 etape 1

---
+++ TP4/ex2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+
+void afficherMenu()
+{
+    printf("=== BANQUE - DISTRIBUTEUR ===\n1 - Faire un retrait \n2 - Quitter\n");
+        }
+
+int main(){
+    int choix = 0;
+    printf("Votre choix : ");
+    scanf("%d", &choix);
+}
