7a1679789a0290885e5bb40e47ca6759d28c39d7
Malo <malo.hamon@student.junia.com>
2025-11-26 16:09:34 +0100

    tp4 ex2 etape 3

--- TP4/ex2.c
+++ TP4/ex2.c
@@ -16,6 +16,17 @@ int saisir_montant(){
     return montant;
 }
 
+int montant_valide(int montant){
+    if (montant%5 == 0 && 5<montant<1000){
+        return 0;
+    }
+    else{
+        printf("Erreur : montant invalide\n Le montant doit etre entre 5 et 1000, divisible par 5.\n");
+        return 1;
+    }
+    
+}
+
 int main(){
     int choix = 0;
     printf("Votre choix : ");
